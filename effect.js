5d3e0f1aef24bf83c45688d5e436cbd2103db1e3
sudaraeb <ericsonsudara@gmail.com>
2020-02-10 14:51:36 +0800

    Update effect.js

--- effect.js
+++ effect.js
@@ -24,7 +24,7 @@ $('document').ready(function(){
 		$('#bulb_pink').addClass('bulb-glow-pink');
 		$('#bulb_orange').addClass('bulb-glow-orange');
 		$('body').addClass('peach');
-		$(this).fadeOut('slow').delay(5000).promise().done(function(){
+		$(this).fadeOut('slow').delay(10000).promise().done(function(){
 			$('#play').fadeIn('slow');
 		});
 	});
@@ -39,7 +39,7 @@ $('document').ready(function(){
 		$('#bulb_orange').addClass('bulb-glow-orange-after');
 		$('body').css('backgroud-color','#FFF');
 		$('body').addClass('peach-after');
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
+		$(this).fadeOut('slow').delay(10000).promise().done(function () {
 			$('#bannar_coming').fadeIn('slow');
 		});
 	});
@@ -49,7 +49,7 @@ $('document').ready(function(){
 
 	$('#bannar_coming').click(function(){
 		$('.bannar').addClass('bannar-come');
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
+		$(this).fadeOut('slow').delay(10000).promise().done(function () {
 			$('#balloons_flying').fadeIn('slow');
 		});
 	});
@@ -122,7 +122,7 @@ $('document').ready(function(){
 		loopSix();
 		loopSeven();
 		
-		$(this).fadeOut('slow').delay(5000).promise().done(function(){
+		$(this).fadeOut('slow').delay(10000).promise().done(function () {
 		    $('#wish_message').fadeIn('slow');
 		});
 	});	
@@ -162,7 +162,7 @@ $('document').ready(function(){
 		$('#b77').animate({top:240, left: vw+250},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
-		$(this).fadeOut('slow').delay(3000).promise().done(function(){
+		$(this).fadeOut('slow').delay(10000).promise().done(function () {
 			$('#story').fadeIn('slow');
 		});
 	});
